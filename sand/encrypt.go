dba3c356d130d5c730c99bd283dc6dadc84d9ff0
yushengguo557 <yushengguo557@gmail.com>
2023-08-16 14:34:24 +0800

    fixed

--- sand/encrypt.go
+++ sand/encrypt.go
@@ -19,6 +19,7 @@ func AESEncrypt(data []byte) (string, string, error) {
 	if err != nil {
 		return "", "", err
 	}
+	fmt.Println("AES Key:", string(aesKey))
 
 	// 1.2 创建 AES 加密块
 	block, err := aes.NewCipher(aesKey)
@@ -27,17 +28,17 @@ func AESEncrypt(data []byte) (string, string, error) {
 	}
 
 	// Generate a random IV with the same size as the block
-	iv := make([]byte, aes.BlockSize)
-	if _, err := rand.Read(iv); err != nil {
-		return "", "", err
-	}
+	//iv := make([]byte, aes.BlockSize)
+	//if _, err := rand.Read(iv); err != nil {
+	//	return "", "", err
+	//}
 
 	// 1.3 对原始数据进行填充
 	padding := aes.BlockSize - (len(data) % aes.BlockSize)
 	paddedData := append(data, bytes.Repeat([]byte{byte(padding)}, padding)...)
 
 	// 1.4 创建 ECB 分组模式
-	mode := cipher.NewCBCEncrypter(block, iv)
+	mode := cipher.NewCBCEncrypter(block, nil)
 
 	// 1.5 加密数据
 	cipherText := make([]byte, len(paddedData))
@@ -63,6 +64,28 @@ func AESEncrypt(data []byte) (string, string, error) {
 	// 3. 返回加密后的结果 包括value和key
 	return encryptValue, sandEncryptKey, nil
 }
+
+//// AESDecrypt AES 解密数据
+//func AESDecrypt(encryptedData []byte, key string) ([]byte, error) {
+//	// 创建 AES 解密块
+//	block, err := aes.NewCipher([]byte(key))
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	// 创建 ECB 分组模式
+//	mode := cipher.NewCBCDecrypter(block, nil)
+//
+//	// 解密数据
+//	decryptedData := make([]byte, len(encryptedData))
+//	mode.CryptBlocks(decryptedData, encryptedData)
+//
+//	// 去除填充字节
+//	padding := decryptedData[len(decryptedData)-1]
+//	decryptedData = decryptedData[:len(decryptedData)-int(padding)]
+//
+//	return decryptedData, nil
+//}
 
 // AESEncrypt AES加密
 //func AESEncrypt(data []byte) (string, string, error) {
